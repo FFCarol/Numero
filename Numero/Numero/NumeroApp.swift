7b688e7886fcd3ed1f63a3a2c107f0b2abc003c7
carolina.soares <carolina.soares@farfetch.com>
2023-10-02 20:14:19 +0100

    First commit

---
+++ Numero/Numero/NumeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumeroApp.swift
+//  Numero
+//
+//  Created by Carolina Soares on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NumeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
