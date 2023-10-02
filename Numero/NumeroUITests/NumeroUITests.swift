42c695729969e14e8ca27e205a46e9eed735a077
carolina.soares <carolina.soares@farfetch.com>
2023-10-02 22:26:35 +0100

    Test

--- Numero/NumeroUITests/NumeroUITests.swift
+++ Numero/NumeroUITests/NumeroUITests.swift
@@ -30,6 +30,12 @@ final class NumeroUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testConversionForTwo() {
+
+      let result = 2 + 2
+      XCTAssertEqual(result, 4, "Sum of 4 is correct")
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
